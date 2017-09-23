b23cf49f50944e852285e704433fb3dc2e516b40
wanghaisheng <edwin_uestc@163.com>
2017-09-24 05:37:30 +0800

    fallback other.py

--- src/other.py
+++ src/other.py
@@ -26,8 +26,8 @@ def draw_boxes(im, bboxes, is_display=True, color=None, caption="Image", wait=Tr
             c=color
         cv2.rectangle(im, tuple(box[:2]), tuple(box[2:4]), c)
     if is_display:
-#        cv2.imshow(caption, im)
-        cv2.imwrite(caption, im)
+        cv2.imshow(caption, im)
+#        cv2.imwrite(caption, im)
 
         if wait:
             cv2.waitKey(0)
